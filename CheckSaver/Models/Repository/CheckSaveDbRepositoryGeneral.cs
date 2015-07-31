68453754269fd0d4a0af06344b51b3f8cad9949d
natashalysakova <natashalysakova@gmail.com>
2015-07-31 18:24:32 +0300

    Update general repository code

--- CheckSaver/Models/Repository/CheckSaveDbRepositoryGeneral.cs
+++ CheckSaver/Models/Repository/CheckSaveDbRepositoryGeneral.cs
@@ -7,30 +7,30 @@ namespace CheckSaver.Models.Repository
 {
     public partial class CheckSaveDbRepository :IDisposable
     {
-        private uh357966_db2Entities _db = new uh357966_db2Entities();
+        private uh357966_db2Entities1 _db = new uh357966_db2Entities1();
 
 
         internal IEnumerable GetStoresList()
         {
-            return (from store in _db.Store select new {Id = store.Id, Title = store.Title});
+            return (from store in _db.n_Stores select new {Id = store.Id, Title = store.Title});
         }
 
         internal IEnumerable GetNeighborsList()
         {
-            return (from neighbor in _db.Neighbor select new { Id = neighbor.Id, Name = neighbor.Name });
+            return (from neighbor in _db.n_Neighbours select new { Id = neighbor.Id, Name = neighbor.Name });
         }
 
 
         public Dictionary<int, string> GetNeighboursNames()
         {
-            return _db.Neighbor.Select(t => new { t.Id, t.Name })
+            return _db.n_Neighbours.Select(t => new { t.Id, t.Name })
                    .ToDictionary(t => t.Id, t => t.Name);
         }
 
-        public List<Product> FindProductsinDb(string term, int storeId)
+        public List<n_Products> FindProductsinDb(string term, int storeId)
         {
-            List<Product> products = (from product in _db.Product
-                where product.Name.ToLower().StartsWith(term.ToLower()) && product.StoreId == storeId
+            List<n_Products> products = (from product in _db.n_Products
+                where product.Title.ToLower().StartsWith(term.ToLower())
                 select product).ToList();
             return products;
         }
