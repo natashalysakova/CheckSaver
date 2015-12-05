b90e050b91cb39d44c1ac0ac4cec3dae3400b88a
natashalysakova <natashalysakova@gmail.com>
2015-12-05 14:36:47 +0200

    Add Unit Test project for testing

---
+++ CheckSaverTestProjects/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CheckSaver.Models.Repository;
+using System.Diagnostics;
+
+namespace CheckSaverTestProjects
+{
+    [TestClass]
+    public class NeighbourTestClass
+    {
+        CheckSaveDbRepository _repository = new CheckSaveDbRepository();
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var res = _repository.GetNeighboursNames();
+
+            foreach (var item in res)
+            {
+                Debug.WriteLine(string.Format("{0}: {1}", item.name, item.isDefault));
+            }
+
+            Assert.AreEqual(true, res[0].isDefault);
+            
+        }
+    }
+}
